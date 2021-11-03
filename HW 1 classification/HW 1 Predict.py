819222e2a27b21bc918049348f10e463efed0feb
oscar <oscar19971003@gmail.com>
2021-11-03 18:22:37 +0800

    PEP8 OK

--- HW 1 classification/HW 1 Predict.py
+++ HW 1 classification/HW 1 Predict.py
@@ -12,6 +12,7 @@ from torchvision import models, transforms
 
 # ------------------------- Hyper Params            -------------------------
 
+# Check Hyper parameters form "HW 1.py"
 BatchSize = 64
 NumWorkers = 4
 
@@ -20,11 +21,11 @@ CropSize = (400, 400)
 
 # ------------------------- PreRequirement          -------------------------
 
-# ModelLoadPath = r'HW 1 classification/MODEL.pt'
-ModelLoadPath = r'HW 1 classification/resnet50.pt'
+ModelLoadPath = r'HW 1 classification/MODEL.pt'
 AnswerSavePath = r'HW 1 classification/answer.txt'
 if os.path.isfile(AnswerSavePath):
-    answer = input(f'existing file at {AnswerSavePath}, are you sure to cover it? [y]|n')
+    answer = input(f'existing file at {AnswerSavePath},' +
+                   'are you sure to cover it? [y]|n')
     if answer in ['', 'n']:
         pass
     else:
@@ -45,7 +46,7 @@ with open(AllLabelPath) as file:
 Labels = np.array(Labels)
 
 # Collect Order of Test Image
-Order = [] # List of Strings
+Order = []  # List of Strings
 with open(TestOrderPath) as file:
     for line in file:
         Order += [line.strip()]
@@ -69,32 +70,35 @@ preprocess = transforms.Compose([
 
 # ------------------------- Dataset / DataLoader    -------------------------
 
+
 class MyDataset(Dataset):
     def __init__(self, filenames, folderpath, transform):
         self.filenames = filenames  # ['1.jpg'
 
         self.folderpath = folderpath
         self.transform = transform
-    
+
     def __getitem__(self, index):
         filename = self.filenames[index]
         img = Image.open(self.folderpath + filename)  # check dimension
 
         if self.transform:
             img = self.transform(img)
-        
+
         return img
 
     def __len__(self):
         return len(self.filenames)
 
+
 test_dataset = MyDataset(Order, TestPath, preprocess)
 test_data_loader = DataLoader(dataset=test_dataset,
-                               batch_size=BatchSize,
-                               shuffle=False,
-                               num_workers=NumWorkers)
+                              batch_size=BatchSize,
+                              shuffle=False,
+                              num_workers=NumWorkers)
 
 # ------------------------- Choose Model            -------------------------
+
 
 def GetModel(read_model_path=None):
     model = None
@@ -106,10 +110,12 @@ def GetModel(read_model_path=None):
 
     return model
 
-wide_resnet50_2 = GetModel(ModelLoadPath)
-wide_resnet50_2 = wide_resnet50_2.to('cuda')
+
+predict_model = GetModel(ModelLoadPath)
+predict_model = predict_model.to('cuda')
 
 # ------------------------- Predict                 -------------------------
+
 
 def ModelPredict(model, test_data):
     '''
@@ -132,8 +138,9 @@ def ModelPredict(model, test_data):
     predict_results = np.concatenate(predict_results)
     return np.array(predict_results).flatten()
 
+
 predict_order = ModelPredict(
-    model=wide_resnet50_2,
+    model=predict_model,
     test_data=test_data_loader
 )
 predict_results = Labels[predict_order]
@@ -144,5 +151,3 @@ for i in range(test_data_size):
     submission.append([Order[i], predict_results[i]])
 
 np.savetxt(AnswerSavePath, submission, fmt='%s')
-
-
